b0f66881b6fd23cf74e48ed1b6cbc318080b3e5d
erdem9d <omer.erdem124@gmail.com>
2024-11-08 13:35:43 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 3;   
+        int n = 4;  
+
+      
+        Console.WriteLine($"A({m}, {n}) = {Ackermann(m, n)}");     
+        Console.WriteLine("Devam etmek için bir tuşa basın...");
+        Console.ReadLine();  
+    }
+
+    
+    static int Ackermann(int m, int n)
+    {
+        
+        if (m == 0)
+            return n + 1;          
+        else if (m == 1)
+            return n + 2;       
+        else if (m > 1 && n == 0)
+            return Ackermann(m - 1, 1);         
+        else if (m > 1 && n > 0)
+            return Ackermann(m - 1, Ackermann(m, n - 1));  
+       throw new ArgumentOutOfRangeException("m and n must be non-negative integers.");
+    }
+}
